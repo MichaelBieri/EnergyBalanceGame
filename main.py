e3c8798bfb3470b3d25710fa88d65767e8ecffd6
TheDidgeri <01didgeri@gmail.com>
2024-06-08 14:59:19 +0200

    next class

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 # EnergyBalanceGame
 # MichaelBieri, Mai 2024
 
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
 import pygame
 import tkinter as tk
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window_width = 1200
+        self.window_height = 600
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        pygame.display.set_caption(("EnergyBalanceGame"))
+        self.clock = pygame.time.Clock()
+        self.run()
 
-pygame.init()
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    running = False
 
-window_width = 1200
-window_height = 600
-window = pygame.display.set_mode((window_width, window_height))
+            self.delta_time = self.clock.tick(60) / 1000
+            self.window.fill((50, 50, 25)) # Farbe Hintergrund
+            pygame.display.update()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+        pygame.quit()
 
-    window.fill((25, 25, 25))
-    pygame.display.update()
-
-pygame.quit()
+game = Game()
