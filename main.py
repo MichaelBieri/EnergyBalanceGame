7547a8950d9d5275e6c23806b0b3566be754993f
TheDidgeri <01didgeri@gmail.com>
2024-06-09 22:24:46 +0200

    Class icon

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # EnergyBalanceGame
 # MichaelBieri, Mai 2024
-
 import pygame
 import sys
 
-relative_position = (0.2, 0.8)  # Ratio of the image position to the relative window size (x, y as a percentage)
+class Icon:
+    def __init__(self, image_path, relative_position, screen_width, screen_height):
+        self.image = pygame.image.load(image_path)
+        self.position = (
+            int(screen_width * relative_position[0] - self.image.get_width() / 2),
+            int(screen_height * relative_position[1] - self.image.get_height() / 2)
+        )
+
+    def draw(self, window):
+        window.blit(self.image, self.position)
 
 class Game:
     def __init__(self):
@@ -15,13 +23,20 @@ class Game:
         pygame.display.set_caption("EnergyBalanceGame")
         self.clock = pygame.time.Clock()
         self.running = True
+
+        # Load and scale the background image
         self.background_image = pygame.image.load('Pictures/EnergyBalanceGame_background.png')
         self.background_image = pygame.transform.scale(self.background_image, (self.screen_width, self.screen_height))
-        self.solar_icon = pygame.image.load('Pictures/EnergyBalanceGame_solar.png')
-        self.position = (
-            int(self.screen_width * relative_position[0] - self.solar_icon.get_width() / 2),
-            int(self.screen_height * relative_position[1] - self.solar_icon.get_height() / 2)
-        )
+
+        # Create icons
+        self.icons = [
+            Icon('Pictures/EnergyBalanceGame_solar.png', (0.2, 0.8), self.screen_width, self.screen_height),
+            Icon('Pictures/EnergyBalanceGame_water.png', (0.3, 0.8), self.screen_width, self.screen_height),
+            Icon('Pictures/EnergyBalanceGame_wind.png', (0.4, 0.8), self.screen_width, self.screen_height),
+            Icon('Pictures/EnergyBalanceGame_nuclear.png', (0.5, 0.8), self.screen_width, self.screen_height),
+            Icon('Pictures/EnergyBalanceGame_storage.png', (0.6, 0.8), self.screen_width, self.screen_height),
+            Icon('Pictures/EnergyBalanceGame_foreign.png', (0.7, 0.8), self.screen_width, self.screen_height)
+        ]
 
     def run(self):
         while self.running:
@@ -41,7 +56,8 @@ class Game:
 
     def render(self):
         self.window.blit(self.background_image, (0, 0))
-        self.window.blit(self.solar_icon, self.position)
+        for icon in self.icons:
+            icon.draw(self.window)
         pygame.display.flip()
 
 # Main program
