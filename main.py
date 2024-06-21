970059327da07203dc48aa81e99bbdbf6c3377cd
TheDidgeri <01didgeri@gmail.com>
2024-06-21 16:40:12 +0200

    Classes in files

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # EnergyBalanceGame
 # MichaelBieri, Mai 2024
+
 import threading
-# EnergyBalanceGame
-# MichaelBieri, Mai 2024
 from welcome_window import WelcomeWindow
 from game import Game
 import pygame
